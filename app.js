80fcaec0a1340bbd40d408f791be7b93d4e81341
Bryan C Guner <bryan.guner@gmail.com>
2023-04-14 09:44:00 -0400

    advantages of express.js

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-const express = require("express"); //this is a function that returns an object
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express(); //this is an object
+const app = express();
 
-app.use((req, res, next) => {
-  let body = "";
-  //req.on is a listener
-  req.on("end", () => {
-    const userName = body.split("=")[1];
-    if (userName) {
-      req.body = { name: userName };
-    }
-    next(); //call next in the end handler bc we want to wait until we have the full body.
-  });
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/user", (req, res, next) => {
+  res.send("<h1>User: " + req.body.username + "</h1>");
 });
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send("<h1>" + req.body.name + "</h1>");
-  }
+app.get("/", (req, res, next) => {
   res.send(
-    "<form method='post'><input type='text' name='username'></input><button type='submit'>Create User</button</form>"
+    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
   );
 });
 
